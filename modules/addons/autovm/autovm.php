7b77fbba64e372229cdacf1c3d8bd2bce7b14434
Salar Khosravi <salar.khosravi@gmail.com>
2023-11-21 20:01:22 +0330

    recover older codes

--- modules/addons/autovm/autovm.php
+++ modules/addons/autovm/autovm.php
@@ -3,9 +3,26 @@
 use WHMCS\Database\Capsule;
 
 function autovm_config()
-{
-    return ['name' => 'AutoVM', 'version' => 'V05.08.00', 'author' => 'autovm.net'];
+{  
+    $BackendUrlLabel = '<span style="padding-left: 30px">Insert your Backend Address, started with http as an example "http://backend.autovm.online"</span>';
+    $AdminTokenLabel = '<span style="padding-left: 30px">Insert your Admin Token here, as an Example "de8fs953k49ho3ellg9x", You can find the Admin Token in AutoVM Frontend Panel/User tab</span>';
+    $DefLangLabel = '<span style="padding-left: 30px">Select a language as default language for admin and users panel, this language setting is for AutoVM Module and has nothing to do with WHMCS language setting</span>';
+    $CloudActivationStatusLabel = '<span style="padding-left: 30px; color: red;">!!! Please be very careful. Enabling Cloud Module unintentionally or accidentally can be harmful.</span>';
+    
+    $configarray = array(
+        "name" => "AutoVM",
+        "description" => "Main AutoVM Module",
+        "version" => "V05.09.00",
+        "author" => "autovm.net",
+        "fields" => array(
+            "BackendUrl" => array ("FriendlyName" => "Backend Url", "Type" => "text", "Size" => "31", "Description" => $BackendUrlLabel, "Default" => "http://backend.autovm.online"),
+            "AdminToken" => array ("FriendlyName" => "Admin Token", "Type" => "text", "Size" => "31", "Description" => $AdminTokenLabel, "Default" => "xxxx"),
+            "DefLang" => array ("FriendlyName" => "Default Language", "Type" => "dropdown", "Options" => "English, Farsi, Turkish, Russian, Deutsch, French", "Description" => $DefLangLabel, "Default" => "English"),
+            "CloudActivationStatus" => array ("FriendlyName" => "Enable Cloud Module", "Type" => "yesno", 'Description' => $CloudActivationStatusLabel, "Default" => ""),
+        ));
+        return $configarray;
 }
+
 
 function autovm_activate()
 {
@@ -32,4 +49,100 @@ function autovm_activate()
             $table->string('machine_id');
         });
     }
+}
+
+
+// Get Token From AutoVm module
+function autovm_get_admintoken_baseurl_autovm(){
+    $response = [];
+
+    // find Module aparams
+    try {
+        $moduleparams = Capsule::table('tbladdonmodules')->get();
+        foreach ($moduleparams as $item) {
+            if($item->module == 'autovm'){
+                if($item->setting == 'BackendUrl'){
+                    $BackendUrl = $item->value;
+                }
+                
+                if($item->setting == 'AdminToken'){
+                    $AdminToken = $item->value;
+                }
+                
+                if($item->setting == 'DefLang'){
+                    $DefLang = $item->value;
+                }
+                
+                if($item->setting == 'CloudActivationStatus'){
+                    $CloudActivationStatus = $item->value;
+                }
+            }
+        }
+    } catch (\Exception $e) {
+        $message = '<p><li style="color:red; padding: 5px;">Database ERR ===> Can not find module params table in database</li></p>';
+        $response['message'] = $message;
+        return $response;
+    }
+
+    if(empty($BackendUrl)){
+        $message = '<p><li style="color:red; padding: 5px;">Backend URL ERR ===> Go to addons module and insert your backend adrress</li></p>';
+        $response['message'] = $message;
+        return $response;
+    }
+    
+    if(empty($AdminToken)){
+        $message = '<p><li style="color:red; padding: 5px;">Admin Token ERR ===> Go to addons module and insert your Token</li></p>';
+        $response['message'] = $message;
+        return $response;
+    }
+
+    if($AdminToken && $BackendUrl && $DefLang){
+        $response['AdminToken'] = $AdminToken;
+        $response['BackendUrl'] = $BackendUrl;
+        $response['DefLang'] = $DefLang;
+        return $response;
+    }
+    
+}
+
+
+// Show in admin panel in addon menu page
+function autovm_output($vars) {
+
+    $response = autovm_get_admintoken_baseurl_autovm();    
+    if(!empty($response['DefLang'])){
+        $DefLang = $response['DefLang'];
+    } else {
+        $DefLang = 'English';
+    }
+
+    $version = $vars['version'];
+    $text = '<h2> Version : ' . $version . '</h2><h3>Language :' .  $DefLang . '</h3>';
+    echo($text);
+
+
+    $text = '
+        <p style="padding: 50px 0px 0px 0px; !important">
+        <span style="font-weight: 800 !important;">AutoVM Module</span> is a specialized module designed by <a href="https://AutoVM.net/" style="font-weight: 800 !important;" target="_blank">AutoVM</a> to streamline the connection between the AutoVM Backend and WHMCS.
+        </p>';
+    echo($text);
+
+    
+    
+    $text = '
+        <p>
+        You can always get the latest version from the <a href="https://github.com/AutoVM-modules/AutoVM-WHMCS-Modules" style="font-weight: 800 !important;" target="_blank">AutoVM git repository</a>
+        </p>
+        <p>
+        To learn how to use AutoVM modules, please check out the <a href="https://AutoVM.net/docs/" style="font-weight: 800 !important;" target="_blank"> AutoVM documentation page</a>
+        </p>
+        ';
+    echo($text);
+
+    
+    if(!empty($response['message'])){
+        echo('<pre style="padding: 20px 20px 20px 20px; margin: 30px 0px 30px 0px">');
+        print_r($response['message']);
+        echo('</pre>');
+    } 
 }
--- modules/addons/cloud/controller.php
+++ modules/addons/cloud/controller.php
@@ -5,11 +5,20 @@ use PG\Request\Request;
 
 class CloudController
 {
+    
     protected $clientId;
+    protected $AdminToken;
+    protected $BackendUrl;
 
-    public function __construct($clientId)
+    public function __construct($clientId, $AdminToken, $BackendUrl)
     {
         $this->clientId = $clientId;
+        $this->AdminToken = $AdminToken;
+        
+        $BackendUrl = str_replace(' ', '', $BackendUrl);
+        $BackendUrl = preg_replace('/\s+/', '', $BackendUrl);        
+        $this->BackendUrl = $BackendUrl;
+        
     }
 
     public function pageIndex()
@@ -44,386 +53,316 @@ class CloudController
     public function sendLoginRequest($token)
     {
         $params = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'auth', 'token', 'login'
+            $BackendUrl, 'candy', 'frontend', 'auth', 'token', 'login'
         ];
-
         return Request::instance()->setAddress($address)->setParams($params)->getResponse()->asObject();
     }
 
     public function machines()
     {
         $token = $this->getUserTokenFromClientId();
-
-        // Send request
         $response = $this->sendMachinesRequest($token);
-
         $this->response($response);
     }
 
     public function sendMachinesRequest($token)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'index'
+            $BackendUrl, 'candy', 'frontend', 'machine', 'index'
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function machine()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendMachineRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendMachineRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'show', $id
+            $BackendUrl, 'candy', 'frontend', 'machine', 'show', $id
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function detail()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendDetailRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendDetailRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'detail', $id
+            $BackendUrl, 'candy', 'frontend', 'machine', 'detail', $id
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function create()
     {
         $params = autovm_get_post_array(['productId', 'templateId', 'publicKey', 'name']);
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendCreateRequest($token, $params);
-
         $this->response($response);
     }
 
     public function sendCreateRequest($token, $params)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'create'
+            $BackendUrl, 'candy', 'frontend', 'machine', 'create'
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->setParams($params)->getResponse()->asObject();
     }
 
     public function setup()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendSetupRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendSetupRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'setup', $id
+            $BackendUrl, 'candy', 'frontend', 'machine', 'setup', $id
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function change()
     {
         $id = autovm_get_query('id');
-
         $params = autovm_get_post_array(['templateId']);
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendChangeRequest($token, $id, $params);
-
         $this->response($response);
     }
 
     public function sendChangeRequest($token, $id, $params)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'change', $id
+            $BackendUrl, 'candy', 'frontend', 'machine', 'change', $id
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->setParams($params)->getResponse()->asObject();
     }
 
     public function start()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendStartRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendStartRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'start', $id
+            $BackendUrl, 'candy', 'frontend', 'machine', 'start', $id
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function stop()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendStopRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendStopRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'stop', $id
+            $BackendUrl, 'candy', 'frontend', 'machine', 'stop', $id
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function reboot()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendRebootRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendRebootRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'reboot', $id
+            $BackendUrl, 'candy', 'frontend', 'machine', 'reboot', $id
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function snapshot()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendSnapshotRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendSnapshotRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'snapshot', $id
+            $BackendUrl, 'candy', 'frontend', 'machine', 'snapshot', $id
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function revert()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendRevertRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendRevertRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'revert', $id
+            $BackendUrl, 'candy', 'frontend', 'machine', 'revert', $id
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function console()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendConsoleRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendConsoleRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'console', $id
+            $BackendUrl, 'candy', 'frontend', 'machine', 'console', $id
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function destroy()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendDestroyRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendDestroyRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'machine', 'destroy', $id
+            $BackendUrl, 'candy', 'frontend', 'machine', 'destroy', $id
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function expenses()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendExpensesRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendExpensesRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'graph', 'machine', 'expenses', $id
+            $BackendUrl, 'candy', 'frontend', 'graph', 'machine', 'expenses', $id
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function currentTrafficUsage()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendCurrentTrafficUsageRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendCurrentTrafficUsageRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'graph', 'machine', $id, 'traffic', 'current'
+            $BackendUrl, 'candy', 'frontend', 'graph', 'machine', $id, 'traffic', 'current'
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function memoryUsage()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendMemoryUsageRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendMemoryUsageRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'graph', 'machine', $id, 'memory', 'daily'
+            $BackendUrl, 'candy', 'frontend', 'graph', 'machine', $id, 'memory', 'daily'
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function cpuUsage()
     {
         $id = autovm_get_query('id');
-
         $token = $this->getUserTokenFromClientId();
-
         $response = $this->sendCpuUsageRequest($token, $id);
-
         $this->response($response);
     }
 
     public function sendCpuUsageRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'graph', 'machine', $id, 'cpu', 'daily'
+            $BackendUrl, 'candy', 'frontend', 'graph', 'machine', $id, 'cpu', 'daily'
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
@@ -441,77 +380,71 @@ class CloudController
     public function sendBandwidthUsageRequest($token, $id)
     {
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'graph', 'machine', $id, 'bandwidth', 'current'
+            $BackendUrl, 'candy', 'frontend', 'graph', 'machine', $id, 'bandwidth', 'current'
         ];
-
         return Request::instance()->setAddress($address)->setHeaders($headers)->getResponse()->asObject();
     }
 
     public function regions()
     {
         $response = $this->sendRegionsRequest();
-
         $this->response($response);
     }
 
     public function sendRegionsRequest()
     {
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'common', 'regions'
+            $BackendUrl, 'candy', 'frontend', 'common', 'regions'
         ];
-
         return Request::instance()->setAddress($address)->getResponse()->asObject();
     }
 
     public function products()
     {
         $regionId = autovm_get_query('id');
-
         $response = $this->sendProductsRequest($regionId);
-
         $this->response($response);
     }
 
     public function sendProductsRequest($regionId)
     {
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'common', 'products', $regionId
+            $BackendUrl, 'candy', 'frontend', 'common', 'products', $regionId
         ];
-
         return Request::instance()->setAddress($address)->getResponse()->asObject();
     }
 
     public function templates()
     {
         $response = $this->sendTemplatesRequest();
-
         $this->response($response);
     }
 
     public function sendTemplatesRequest()
     {
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'common', 'templates'
+            $BackendUrl, 'candy', 'frontend', 'common', 'templates'
         ];
-
         return Request::instance()->setAddress($address)->getResponse()->asObject();
     }
 
     public function categories()
     {
         $response = $this->sendCategoriesRequest();
-
         $this->response($response);
     }
 
     public function sendCategoriesRequest()
     {
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'frontend', 'common', 'template', 'categories'
+            $BackendUrl, 'candy', 'frontend', 'common', 'template', 'categories'
         ];
-
         return Request::instance()->setAddress($address)->getResponse()->asObject();
     }
 
@@ -520,32 +453,24 @@ class CloudController
         $params = [
             'userId' => $this->clientId
         ];
-
         $user = Capsule::selectOne('SELECT token FROM autovm_user WHERE user_id = :userId', $params);
-
-        // The first value
         if ($user) {
             return current($user);
         }
-
         return $user;
     }
 
     public function response($response)
     {
         header('Content-Type: application/json');
-
         $response = json_encode($response);
-
         exit($response);
     }
 
     public function handle($action)
     {
         $class = new ReflectionClass($this);
-
         $method = $class->getMethod($action);
-
         if ($method) {
             return $method->invoke($this);
         }
@@ -647,7 +572,7 @@ class CloudController
             echo 'can not access charge amount (E03-Charge Cloud)';
         }
 
-        $token = AUTOVM_ADMIN_TOKEN ;
+        $token = $this->AdminToken;;
     
         $response = $this->sendChargeCloudRequest($token, $userId, $chargeamount);
         $this->response($response);
@@ -663,9 +588,9 @@ class CloudController
         ];
 
         $headers = ['token' => $token];
-
+        $BackendUrl = $this->BackendUrl;
         $address = [
-            AUTOVM_BASE, 'candy', 'backend', 'trans', 'create'
+            $BackendUrl, 'candy', 'backend', 'trans', 'create'
         ];
 
         return Request::instance()->setAddress($address)->setHeaders($headers)->setParams($params)->getResponse()->asObject();
