b0b75302980faa5e1c14a5759276bb97366639bf
Salar Khosravi <salar.khosravi@gmail.com>
2023-11-29 14:41:35 +0330

    feat: add admin panel config into module config

--- modules/addons/cloud/views/autovm/machine.php
+++ modules/addons/cloud/views/autovm/machine.php
@@ -1,20 +1,20 @@
-<?php  include('./includes/commodules/header.php');   ?>
+<?php  include_once('./includes/commodules/header.php');   ?>
 
 <body>
 <!-- main container -->
 <div class="machineapp container-fluid p-4 my-5" style="max-width: 1350px; padding-bottom: 200px !important;" v-cloak>
-    <?php  include('./includes/commodules/backflash.php');     ?>
+    <?php  include_once('./includes/commodules/backflash.php');     ?>
     <div class="row bg-white border border-1 border-secondar rounded-4 py-5 my-5 px-2 px-md-4 px-lg-5">
-        <?php  include('./includes/machineparts/modalaction.php');      ?>
-        <?php  include('./includes/machineparts/modalconsole.php');     ?>
-        <?php  include('./includes/machineparts/modalprocessing.php');  ?>
-        <?php  include('./includes/machineparts/modalsetup.php');       ?>
-        <?php  include('./includes/machineparts/modaldestroy.php');     ?>
-        <?php  include('./includes/machineparts/hoststatus.php');       ?>
-        <?php  include('./includes/machineparts/machinedetails.php');   ?>
-        <?php  include('./includes/machineparts/login.php');            ?>
-        <?php  include('./includes/machineparts/circlures.php');        ?>
-        <?php  include('./includes/machineparts/access.php');           ?>
+        <?php  include_once('./includes/machineparts/modalaction.php');      ?>
+        <?php  include_once('./includes/machineparts/modalconsole.php');     ?>
+        <?php  include_once('./includes/machineparts/modalprocessing.php');  ?>
+        <?php  include_once('./includes/machineparts/modalsetup.php');       ?>
+        <?php  include_once('./includes/machineparts/modaldestroy.php');     ?>
+        <?php  include_once('./includes/machineparts/hoststatus.php');       ?>
+        <?php  include_once('./includes/machineparts/machinedetails.php');   ?>
+        <?php  include_once('./includes/machineparts/login.php');            ?>
+        <?php  include_once('./includes/machineparts/circlures.php');        ?>
+        <?php  include_once('./includes/machineparts/access.php');           ?>
     </div>
 </div>
-<?php  include('./includes/commodules/footer.php');           ?>
+<?php  include_once('./includes/commodules/footer.php');           ?>
