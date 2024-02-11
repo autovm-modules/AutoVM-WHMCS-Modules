295479b03ef512f30a5896eb5b40b6435095d82c
sali <salar.khosravi@gmail.com>
2024-02-11 18:30:35 +0330

    index

---
+++ modules/addons/cloudsnp/views/autovm/index.php
@@ -0,0 +1,14 @@
+<?php  include('routeconfig.php');   ?>
+<?php  include('./includes/commodules/header.php');   ?>
+<body class="container-fluid m-0 p-0" style="background-color: #ff000000 !important;">
+
+<div class="row py-5" >
+    <div class="indexapp col-12">
+        <div class="bg-white rounded-4 border border-2 border-body-secondary" v-cloak>
+            <?php include('./includes/indexparts/headtitle.php');     ?>
+            <?php include('./includes/indexparts/machineslist.php');  ?>              
+        </div>
+    </div>
+</div> 
+
+<?php include('./includes/commodules/footer.php'); ?>
