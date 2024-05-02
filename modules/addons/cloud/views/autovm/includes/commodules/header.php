86dbdcbc8e28a9f14a416595e2700d4bac5ec50f
Saeed Johari <59645534+persgeek@users.noreply.github.com>
2024-05-02 09:41:52 +0330

    Update bootstrap icons

--- modules/addons/cloud/views/autovm/includes/commodules/header.php
+++ modules/addons/cloud/views/autovm/includes/commodules/header.php
@@ -8,7 +8,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="<?php echo($PersonalRootDirectoryURL); ?>/modules/addons/cloud/views/autovm/includes/assets/js/bootstrap.bundle.min.js"></script>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.css">
 
         <style>
             [v-cloak] { display: none; }
